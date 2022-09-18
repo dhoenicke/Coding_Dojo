3118c6abc3040f2ec9888001c06a115ff62b3b81
Daniel <djhoenicke@gmail.com>
2022-09-18 13:56:26 -0500

    save changes

--- Day 5/Fizzbuzz Assignment/Fizzbuzz Assignment.js
+++ Day 5/Fizzbuzz Assignment/Fizzbuzz Assignment.js
@@ -4,7 +4,7 @@ for (let index = 1; index < 101; index++) {
     if (index % 3 == 0) result += 'Fizz';
     if (index % 5 == 0) result += 'Buzz';
 
-    if (result == '') result index.toString();
+    if (result == '') result: index.toString();
 
     console.log(result);
 }
