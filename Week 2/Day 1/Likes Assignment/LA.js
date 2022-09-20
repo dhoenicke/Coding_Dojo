91970dba187d68304c76d7a04a76bb748ebde6c9
Daniel <djhoenicke@gmail.com>
2022-09-20 18:04:59 -0500

    save changes

--- Week 2/Day 1/Likes Assignment/LA.js
+++ Week 2/Day 1/Likes Assignment/LA.js
@@ -3,4 +3,14 @@ var like = 1;
 function likecount() {
     var num = document.querySelector(".likecount");
     num.innerText = parseFloat(num.innerText) + 1;
-    }
+}
+
+function likeamount() {
+    var num = document.querySelector(".likeamount");
+    num.innerText = parseFloat(num.innerText) + 1;
+}
+
+function likenumber() {
+    var num = document.querySelector(".likenumber");
+    num.innerText = parseFloat(num.innerText) + 1;
+}
