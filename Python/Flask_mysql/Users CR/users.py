e95067ca6caa58c170a084d97e9749d56adf96a1
Daniel <djhoenicke@gmail.com>
2022-10-06 14:01:48 -0500

    Commit to GitHub

--- Python/Flask_mysql/Users CR/users.py
+++ Python/Flask_mysql/Users CR/users.py
@@ -1,4 +1,3 @@
-from unittest import result
 from mysqlconnection import connectToMySQL
 
 class User:
@@ -9,6 +8,11 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
 
     @classmethod
     def get_all(cls):
@@ -28,7 +32,17 @@ class User:
         return result
 
     @classmethod
-    def get_one(cls, data):
-        query = "SELECT * FROM users WHERE id = %(id)s";
+    def get_one(cls,data):
+        query  = "SELECT * FROM users WHERE id = %(id)s";
         result = connectToMySQL('users_schema').query_db(query,data)
         return cls(result[0])
+
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s,updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
+
+    @classmethod
+    def destroy(cls,data):
+        query  = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('users_schema').query_db(query,data)
