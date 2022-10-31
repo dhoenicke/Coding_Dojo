2c42bcaa7b54cad88e0ab88d3a5fc9baf0b2c7a5
Daniel <djhoenicke@gmail.com>
2022-10-31 10:02:26 -0500

    Commit to GitHub

---
+++ Mern Week 1/Practice Assignments/hook_form/form/src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+    <Form />
+    </div>
+  );
+}
+
+export default App;
