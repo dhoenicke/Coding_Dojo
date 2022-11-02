9b87b29518b06be7b1996cfcf371c34f275ea6f0
Daniel <djhoenicke@gmail.com>
2022-11-02 11:03:16 -0500

    Commit to GitHub

---
+++ Mern Week 2/Core Assignments/to_do_list/list/src/components/Todo.jsx
@@ -0,0 +1,33 @@
+const Todo = (props) => {
+    const todoClasses = ["bold", "italic"];
+
+    if (props.todo.complete) {
+        todoClasses.push("line-through");
+    }
+
+    return (
+        <div>
+            <input
+                onChange={(event) => {
+                    props.handleToggleComplete
+                    (props.i);
+                }}
+                checked={props.todo.complete}
+                type="checkbox"
+            />
+            <span className={todoClasses.
+            join(" ")}>{props.todo.text}</span>
+            <button
+                onClick={(event) => {
+                    props.handleTodoDelete
+                    (props.i);
+                }}
+                style={{ marginleft: "10px" }}
+            >
+                Delete
+            </button>
+        </div>
+    );
+};
+
+export default Todo;
