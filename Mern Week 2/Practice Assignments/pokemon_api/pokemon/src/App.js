7f52b62b2d8e61d859780a97cfe28838b5cc7c17
Daniel <djhoenicke@gmail.com>
2022-11-02 12:02:44 -0500

    Commit to GitHub

---
+++ Mern Week 2/Practice Assignments/pokemon_api/pokemon/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FetchAPI from './components/FetchAPI';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FetchAPI />
+      </header>
+    </div>
+  );
+}
+
+export default App;
