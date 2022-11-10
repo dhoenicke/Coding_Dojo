96bd9fbc038f83bcf1515d0bf7b2b4e04f3bc5b4
Daniel <djhoenicke@gmail.com>
2022-11-10 15:26:24 -0600

    Commit to GitHub

--- Mern Week 3/MernExam/server/config/mongoose.config.js
+++ Mern Week 3/MernExam/server/config/mongoose.config.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const database = 'MernExam';
+const database = 'Authors';
 
 mongoose.connect(`mongodb://127.0.0.1/${database}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useFindAndModify: false
 })
-.then(() => console.log(`Establised connection to MernExam`))
-.catch((error) => console.log(`Error establishing a connection to MernExam`, error))
+.then(() => console.log(`Establised connection to Authors`))
+.catch((error) => console.log(`Error establishing a connection to Authors`, error))
