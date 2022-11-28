b40b169301b09fe83905b370f830e4e51dad07f1
Daniel <djhoenicke@gmail.com>
2022-11-28 15:45:25 -0600

    Commit to GitHub

---
+++ CSharp Week 1/Assignments/FundamentalsOne/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+// int start = 1;
+// int end = 255;
+// // loop from start to end including end
+// for (int i = start; i <= end; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// Random rand = new Random();
+// for(int i = 0; i <= 5; i++)
+// {
+//     Console.WriteLine(rand.Next(10,21));
+// }
+
+// for(int i = 1; i < 101; i++)
+// {
+//     if(i % 3 == 0 && i % 5 == 0)
+//     {
+//         continue;
+//     }
+//     if(i % 3 == 0 || i % 5 == 0)
+//     {
+//         Console.WriteLine(i);
+//     }
+// }
+
+// for(int i = 1; i < 101; i++)
+// {
+//     if(i % 3 == 0 && i % 5 == 0)
+//     {
+//         continue;
+//     }
+//     if(i % 3 == 0)
+//     {
+//         Console.WriteLine($"{i} - Fizz");
+//     }
+//     if(i % 5 == 0)
+//     {
+//         Console.WriteLine($"{i} - Buzz");
+//     }
+// }
+
+// for(int i = 1; i < 101; i++)
+// {
+//     if(i % 3 == 0 && i % 5 == 0)
+//     {
+//         Console.WriteLine($"{i} - FizzBuzz");
+//     }
+//     if(i % 3 == 0)
+//     {
+//         Console.WriteLine($"{i} - Fizz");
+//     }
+//     if(i % 5 == 0)
+//     {
+//         Console.WriteLine($"{i} - Buzz");
+//     }
+// }
+
+// int j = 1;
+// while(j < 101)
+// {
+//     if(j % 3 == 0 && j % 5 == 0)
+//     {
+//         Console.WriteLine($"{j} - FizzBuzz");
+//     }
+//     if(j % 3 == 0)
+//     {
+//         Console.WriteLine($"{j} - Fizz");
+//     }
+//     if(j % 5 == 0)
+//     {
+//         Console.WriteLine($"{j} - Buzz");
+//     }
+//     j++;
+// }
