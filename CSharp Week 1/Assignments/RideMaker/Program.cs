1f0e95b342d8b238f1354059c2d10be6008875cb
Daniel <djhoenicke@gmail.com>
2022-11-29 14:56:40 -0600

    Commit to GitHub

---
+++ CSharp Week 1/Assignments/RideMaker/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Create 4 vehicles using each constructor at least once
+Vehicle car = new Vehicle("Hyndia Tucson", "Black");
+Vehicle bicycle = new Vehicle("Unknown", 1, "Black", false, 30);
+Vehicle horse = new Vehicle("Cochise", 2, "Spotted", false, 20);
+Vehicle scooter = new Vehicle("Scooter", 1, "Black", true, 15);
+
+// Put all the vehicles created into a List
+List<Vehicle> AllVehicles = new List<Vehicle>();
+AllVehicles.Add(car);
+AllVehicles.Add(bicycle);
+AllVehicles.Add(horse);
+AllVehicles.Add(scooter);
+
+// Loop through the List and have each vehicle run its ShowInfo() method
+foreach (Vehicle v in AllVehicles)
+{
+    v.ShowInfo();
+}
+
+
+// Make one of the vehicles travel 100 miles
+car.Travel(100);
+
+// Print the information of the vehicle to verify the distance travelled went up
+car.ShowInfo();
