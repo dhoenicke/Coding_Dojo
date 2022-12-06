cbca1cdc446cd58eb7958f5ffa4cdf07bdff1e8f
Daniel <djhoenicke@gmail.com>
2022-12-06 11:10:39 -0600

    Commit to GitHub

--- CSharp Week 2/Assignments/LINQEruption/Program.cs
+++ CSharp Week 2/Assignments/LINQEruption/Program.cs
@@ -1,21 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 List<Eruption> eruptions = new List<Eruption>()
 {
-    new Eruption("La Palma", 2021, "Canary Is", 2426, "Stratovolcano"),
-    new Eruption("Villarrica", 1963, "Chile", 2847, "Stratovolcano"),
-    new Eruption("Chaiten", 2008, "Chile", 1122, "Caldera"),
-    new Eruption("Kilauea", 2018, "Hawaiian Is", 1122, "Shield Volcano"),
-    new Eruption("Hekla", 1206, "Iceland", 1490, "Stratovolcano"),
-    new Eruption("Taupo", 1910, "New Zealand", 760, "Caldera"),
-    new Eruption("Lengai, Ol Doinyo", 1927, "Tanzania", 2962, "Stratovolcano"),
-    new Eruption("Santorini", 46, "Greece", 367, "Shield Volcano"),
-    new Eruption("Katla", 950, "Iceland", 1490, "Subglacial Volcano"),
-    new Eruption("Aira", 766, "Japan", 1117, "Stratovolcano"),
-    new Eruption("Ceboruco", 930, "Mexico", 2280, "Stratovolcano"),
-    new Eruption("Etna", 1329, "Italy", 3320, "Stratovolcano"),
-    new Eruption("Bardarbunga", 1477, "Iceland", 2000, "Stratovolcano")
+    new ("La Palma", 2021, "Canary Is", 2426, "Stratovolcano"),
+    new ("Villarrica", 1963, "Chile", 2847, "Stratovolcano"),
+    new ("Chaiten", 2008, "Chile", 1122, "Caldera"),
+    new ("Kilauea", 2018, "Hawaiian Is", 1122, "Shield Volcano"),
+    new ("Hekla", 1206, "Iceland", 1490, "Stratovolcano"),
+    new ("Taupo", 1910, "New Zealand", 760, "Caldera"),
+    new ("Lengai, Ol Doinyo", 1927, "Tanzania", 2962, "Stratovolcano"),
+    new ("Santorini", 46, "Greece", 367, "Shield Volcano"),
+    new ("Katla", 950, "Iceland", 1490, "Subglacial Volcano"),
+    new ("Aira", 766, "Japan", 1117, "Stratovolcano"),
+    new ("Ceboruco", 930, "Mexico", 2280, "Stratovolcano"),
+    new ("Etna", 1329, "Italy", 3320, "Stratovolcano"),
+    new ("Bardarbunga", 1477, "Iceland", 2000, "Stratovolcano")
 };
 // Example Query - Prints all Stratovolcano eruptions
 IEnumerable<Eruption> stratovolcanoEruptions = eruptions.Where(c => c.Type == "Stratovolcano");
