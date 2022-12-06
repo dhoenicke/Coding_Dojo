d40b7c7109fa0ac06729c4de3d7fde3761a0a464
Daniel <djhoenicke@gmail.com>
2022-12-06 16:38:39 -0600

    Commit to GitHub

---
+++ CSharp Week 2/Assignments/FirstConnection/Migrations/20221206223622_SecondMigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FirstConnection.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasFur",
+                table: "Pets",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasFur",
+                table: "Pets");
+        }
+    }
+}
